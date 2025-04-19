32c30b09aaf12d738872021f9207b18f923fa3f4
YuriRomao1 <yurijj@hotmail.com>
2025-04-19 10:42:45 -0300

    Rota para login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { NavComponent } from './components/nav/nav.component';
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
 import { TecnicoComponent } from './components/tecnico/tecnico.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
+  {path: '', component: LoginComponent},
   {
     path: '', component: NavComponent, children: [
       { path: 'home', component: HomeComponent },
