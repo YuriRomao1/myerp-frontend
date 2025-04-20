fc461ab6512f375446c1d6996478bbb3b48c6a31
YuriRomao1 <yurijj@hotmail.com>
2025-04-19 21:13:29 -0300

    Validação de formulário de login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,23 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-
+import { Credenciais } from 'src/app/model/credenciais';
+import { FormControl, FormsModule, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
   imports: [
     CommonModule,
     RouterModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+ creds : Credenciais = {
+    email: '',
+    senha: ''
+ }
+  email = new FormControl(null, Validators.email);
+  senha = new FormControl(null, Validators.minLength(3));
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  validaCampos(): boolean {
+    if(this.email.valid && this.senha.valid) {
+      return true;
+    }
+    return false;
+
+  }
 
 }
