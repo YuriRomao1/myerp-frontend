aaebfd6431eebd89053e4fe507cceeda40b16b7a
YuriRomao1 <yurijj@hotmail.com>
2025-04-22 17:52:42 -0300

    HTML e CSS para componente Tecnico-create

--- src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-tecnico-create',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './tecnico-create.component.html',
   styleUrl: './tecnico-create.component.css'
 })
