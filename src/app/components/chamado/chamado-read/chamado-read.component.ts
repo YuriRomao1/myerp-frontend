a0c41e0e42a47599dd2b1bd90f996bb455a5ca07
YuriRomao1 <yurijj@hotmail.com>
2025-04-26 20:06:08 -0300

    componente chamado-read criado

---
+++ src/app/components/chamado/chamado-read/chamado-read.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chamado-read',
+  imports: [],
+  templateUrl: './chamado-read.component.html',
+  styleUrl: './chamado-read.component.css'
+})
+export class ChamadoReadComponent {
+
+}
