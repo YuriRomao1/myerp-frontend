947d8c99141f73424fa1d1ee58b5b6c80cda7ea4
YuriRomao1 <yurijj@hotmail.com>
2025-04-27 01:45:58 -0300

    atualização

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -32,7 +32,7 @@ export class NavComponent {
     private toast: ToastrService) {}
 
   ngOnInit() {
-    this.router.navigate(['chamados/read/1']);
+    this.router.navigate(['home']);
   }
 
   logout() {
